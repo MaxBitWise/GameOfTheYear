94949d5eb22a17f5bf7087fe67ddbd4e80e7a3f8
Kapuciner <ovalex123@gmail.com>
2021-07-02 03:02:54 +0500

    New Branch, new begining
    Alex: GUI
    Max: Groups

---
+++ Input/KeyboardInput.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace GameTrench
+{
+    static class KeyboardInput
+    {
+        public static void checkCreationGroup()
+        {
+            KeyboardState keystate = Keyboard.GetState();
+            if (keystate.IsKeyDown(Keys.Y) == true)
+            {
+                Globals.creatGroup = true;
+            }else if (keystate.IsKeyDown(Keys.N) == true)
+            {
+                Globals.creatGroup = false;
+                Globals.wasSelected = false;
+            }
+        }
+    }
+}
---
+++ Units/Unit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+
+namespace GameTrench
+{
+    public class Unit
+    {
+        public Vector2 position;
+        public bool side;
+        public int hp;
+        public Unit()
+        {
+            
+        }
+    }
+}
